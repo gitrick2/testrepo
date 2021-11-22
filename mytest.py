d7646b4afb376cfaa3c2749d3b7dd1cd1503d221
gitrick2 <94807939+gitrick2@users.noreply.github.com>
2021-11-22 09:55:04 -0500

    Update mytest.py

--- mytest.py
+++ mytest.py
@@ -2,25 +2,9 @@
 import sys
 print("   sys.executable location -> ", sys.executable)
 
-
 # test: NumPy
 try:
     import numpy
     print("   numpy.__version__ = ", numpy.__version__)
 except:
     print("ERROR: numpy package")
-
-# test: Panda
-try:
-    import pandas as pd
-    print("   pandas.__version__ = ", pd.__version__)
-except:
-    print("ERROR: panda package")
-
-
-# test: Tensorflow
-try:
-    import tensorflow as tf
-    print("   tf.__version__ = ", tf.__version__)
-except:
-    print("ERROR: tensorflow package")
